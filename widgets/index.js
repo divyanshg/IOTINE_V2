616a1f9951ba5fe6911e49e0a0e5f8ba70cf2490
divyanshg <divyanshg809@gmail.com>
2020-04-28 01:00:06 +0530

    major

--- widgets/index.js
+++ widgets/index.js
@@ -104,7 +104,7 @@ var getWidgets = (user, app) => {
 
 var saveWidget = (user, app, widget) => {
     var widgets = [
-        [null, widget.user, widget.app, widget.name, widget.datasets.feed, widget.datasets.label, widget.datasets.data, widget.datasets.backgroundColor, widget.datasets.borderColor, widget.datasets.borderWidth, widget.config.labels, widget.config.type, widget.config.prevTime, widget.config.device, widget.config.tab]
+        [null, widget.user, widget.app, widget.name, widget.feed, widget.type,widget.datasets.label, widget.datasets.data, widget.datasets.backgroundColor, widget.datasets.borderColor, widget.datasets.borderWidth, widget.config.labels, widget.config.type, widget.config.prevTime, widget.config.device, widget.config.tab]
     ]
     con.query("INSERT INTO `widgets`(`id`, `user`, `app`, `name`, `feed`, `type`, `label`, `data`, `backgroundColor`, `borderColor`, `borderWidth`, `labels`, `chartType`, `prevTime`, `device`, `tab`) VALUES ?", [widgets], (err, res) => {
         if(err) throw err;

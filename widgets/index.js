58418d3dfdc1eb6de132f2671e6889b6b0755b6e
divyanshg <divyanshg809@gmail.com>
2020-04-28 00:40:29 +0530

    major

--- widgets/index.js
+++ widgets/index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 3002;
 

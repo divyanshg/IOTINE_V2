42b8dfd7e6c137fac1d2d26b0ca907e2bf8fdc2e
divyanshg <divyanshg809@gmail.com>
2020-04-27 23:42:16 +0530

    changed

--- widgets/index.js
+++ widgets/index.js
@@ -22,7 +22,7 @@ con.connect(function(err) {
 app.get('/widgets/:userId/:appId', (req, res) => {
     con.query("select * from widgets", function (err, result) {
         if (err) throw err;
-        console.log("Result: " + json.stringify(result));
+        console.log("Result: " + JSON.stringify(result));
     });
     res.json(
         [

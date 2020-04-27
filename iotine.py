6b6763a8e77928ec6cf78f4bf208be29144fd25d
divyanshg <divyanshg809@gmail.com>
2020-04-27 16:43:51 +0530

    test

---
+++ iotine.py
@@ -0,0 +1,79 @@
+import paho.mqtt.client as mqtt #import the client1
+import os
+import time
+import random
+import RPi.GPIO as GPIO
+import math
+import json
+import string
+from Crypto.Cipher import AES
+
+obj = AES.new('Let this be a Key something more random to make this key super long', AES.MODE_CBC, 'This is an IV456')
+#Fernet.generate_key()
+f = Fernet(key)
+
+GPIO = GPIO
+IOTINE_HOST="192.168.31.249"
+CONNSTRING = ''
+DEVICENAME = ''
+
+if IOTINE_HOST != "192.168.31.249":
+    print("CUSTOM HOST IS NOT SUPPORTED!/nSITCHING BACK TO IOTINE_HOST")
+    IOTINE_HOST = "192.168.31.249"
+
+def on_message(client, userdata, message):
+    msg = str(message.payload.decode("utf-8"))
+    print(msg)
+
+def on_connect(client, userdata, flags, rc):
+    if rc==0:
+        print("Connection Authorized")
+    else:
+        print("Connection Rejected")
+########################################
+client = mqtt.Client(DEVICENAME) #create new instance
+client.on_message = on_message
+client.on_connect = on_connect
+
+def CONNECT():
+    client.username_pw_set(CONNSTRING)
+    client.connect_async(IOTINE_HOST) #connect to broker
+    client.loop_start() #start the loop
+    
+    subscribe(CONNSTRING+"/$SYS/COMMANDS", '') 
+    #return json.dumps({"status": "Connected"})
+
+def will(topic, payload=None, qos=0, retain=False):
+    client.will_set(topic, payload=payload, qos=qos, retain=retain)
+
+def publish(feed, val, callback=None):
+    if callback == '':
+        client.publish(CONNSTRING+"/"+feed, str(val))
+    else:    
+        client.publish(CONNSTRING+"/"+feed, str(val), callback=callback)
+
+def subscribe(feed, callback=None):
+    if callback == "":
+        return client.subscribe(CONNSTRING+"/"+feed+"/NON")
+    else:
+        client.on_message = callback
+        client.subscribe(CONNSTRING+"/"+feed+"/NON")
+
+def is_published(topic):
+    return topic.is_published()
+
+def wait_for_publish(topic):
+    return topic.wait_for_publish()
+
+def UNSUBSCRIBE(topic, callback=None):
+    if callback == '':
+        client.unsubscribe(topic)
+    else:
+        callback(client.unsubscribe(topic))
+        
+def DISCONNECT(callback):
+    if callback == '':
+        client.disconnect()
+    else:
+        client.disconnect()
+              
---
+++ subnew.py
@@ -0,0 +1,41 @@
+import iotine as IOTINE
+from iotine import GPIO
+import random
+import time
+import requests as req
+
+IOTINE.CONNSTRING = "qwerty12"
+IOTINE.DEVICENAME = "qwerty12"
+
+print(IOTINE.CONNECT())
+
+val = 30
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+
+def on_message(client, userdata, message):
+    msg = str(message.payload.decode("utf-8"))
+    if message.topic.split("/")[1] == "else":
+        r = req.get("http://192.168.31.212:8080/ptz?zoom="+msg)
+    elif message.topic.split("/")[1] == "etg54":    
+        if msg == "ON":
+            r = req.get('http://192.168.31.212:8080/enabletorch')
+            print(r.text)[0:300]
+        #GPIO.output(17, GPIO.HIGH)
+        elif msg == "OFF":
+            r = req.get('http://192.168.31.212:8080/disabletorch')
+            print(r.text)[0:300]    
+    elif message.topic.split("/")[1] == "retg54":    
+        if msg == "ON":
+            GPIO.output(17, GPIO.HIGH)
+        elif msg == "OFF":
+            GPIO.output(17, GPIO.LOW)  
+    print({"topic":message.topic, "message":msg})
+
+while True:
+    IOTINE.publish("retg54", random.randint(-30, 30), '')
+    #IOTINE.publish("Web_Sensor", random.randint(-3, 3), '')
+    IOTINE.subscribe("retg54", on_message)
+    #IOTINE.publish("else", str(random.randint(0, 100)), '')
+    time.sleep(3)

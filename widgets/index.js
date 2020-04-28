1009a82a8c7a992b3e6a775ec3767f1497984793
divyanshg <divyanshg809@gmail.com>
2020-04-28 22:57:59 +0530

    sd

--- widgets/index.js
+++ widgets/index.js
@@ -221,6 +221,7 @@ var saveTemplate =  (user, name) => {
 
 var getFeeds = (user,dev,templ) => {
     return new Promise((resolve, reject) => {
+        console.log(user)
             con.query('select name from feed_vals where user_id = ?',[user], (err, feeds) => {
                 if(err) return reject(err);
                 resolve(feeds)

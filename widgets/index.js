22702554baf17802ed2a3df0e511eeff8f0e593b
divyanshg <divyanshg809@gmail.com>
2020-04-28 22:49:09 +0530

    no

--- widgets/index.js
+++ widgets/index.js
@@ -221,7 +221,7 @@ var saveTemplate =  (user, name) => {
 
 var getFeeds = (user,dev,templ) => {
     return new Promise((resolve, reject) => {
-        con.query("select user_id from users where username = ? limit 1", [user], (err, res) => {
+        con.query("select * from users where username = ?", [user], (err, res) => {
             if(err) return reject(err);
             console.log(res);resolve(res)
             /*con.query('select name from feed_vals where user_id = ? and deviceID = ?', [user, res[0].deviceID], (err, feeds) => {

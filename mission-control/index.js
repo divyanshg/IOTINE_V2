e230717bd6ee887f0c6263717255275df29a0e24
divyanshg <divyanshg809@gmail.com>
2020-05-01 01:41:59 +0530

    test11

--- mission-control/index.js
+++ mission-control/index.js
@@ -65,6 +65,7 @@ io.on('connection', function (socket) {
     });
 
     socket.on('devStat', (device, status) => {
+        console.log(device)
         con.query('select * from devices where deviceID = ?', [device], (err, res) => {
             if(err) throw err;
             console.log(device)

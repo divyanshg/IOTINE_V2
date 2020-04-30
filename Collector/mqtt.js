b1101546265a208b710a2b765c8b7f2de98f9b44
divyanshg <divyanshg809@gmail.com>
2020-05-01 02:13:15 +0530

    test

--- Collector/mqtt.js
+++ Collector/mqtt.js
@@ -76,6 +76,6 @@ server.on('published', (packet) => {
 server.on("clientDisconnected", function (client) {
     con.query('update devices set status = "OFFLINE" where deviceID = ?', [client.id], (err, res) => {
         if (err) throw err;
-        sockClient.emit('devStat', topic[2], "OFFLINE")
+        sockClient.emit('devStat', client.id, "OFFLINE")
     })
 });

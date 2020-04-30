2bbc468996523c6b925ddf6fbadda6636f385825
divyanshg <divyanshg809@gmail.com>
2020-04-30 13:50:26 +0530

    new deploy

--- subnew.py
+++ subnew.py
@@ -38,6 +38,6 @@ while True:
     IOTINE.publish("retg54", random.randint(-30, 30), '')
     IOTINE.publish("Web_Sensor", random.randint(-3, 3), '')
     IOTINE.subscribe("retg54", on_message)
-    IOTINE.publish("else", str(random.randint(0, 100)), '')
+    IOTINE.publish("ENGINE_TEMPERATURE", str(random.randint(0, 200)), '')
     IOTINE.publish("ENGINE_OIL", str(random.randint(0, 100)), '')
     time.sleep(1)

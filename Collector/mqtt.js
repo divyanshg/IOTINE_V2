e5983fa53cf29ea0b137c39d7b0c73818fd95577
divyanshg <divyanshg809@gmail.com>
2020-05-01 01:15:20 +0530

    test

--- Collector/mqtt.js
+++ Collector/mqtt.js
@@ -61,6 +61,8 @@ server.on('published', (packet) => {
             time: new Date().toLocaleTimeString()
         })
         //dataCamp.updateFeed('iub54i6bibu64', 'SkNCX1RSVUNLXzAxYWFk', 'retg54', message)
+    }else if(topic[0] == "$SYS" && topic[1] == "status"){
+        console.log(topic[0]+" : IS NOW ONLINE")
     } else {
         return
     }

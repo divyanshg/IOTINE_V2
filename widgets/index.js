cc1b49eb70e4c96486b3bd1f0dcf2d13e5eef56d
divyanshg <divyanshg809@gmail.com>
2020-04-30 23:12:26 +0530

    test8

--- widgets/index.js
+++ widgets/index.js
@@ -183,7 +183,7 @@ var getWidgets = (user, app) => {
 
 var updateWidget = (user, app, widget) => {
     return new Promise((resolve, reject) => {
-        con.query('UPDATE widgets SET name = ?, feed = ?, backgroundColor = ?, borderColor = ?, borderWidth = ?, device = ? WHERE id = ? ', [widget.name, widget.feed, widget.datasets[0].backgroundColor, widget.datasets[0].borderColor, widget.datasets[0].borderWidth, widget.config.device, widget.id], (err, res) => {
+        con.query('UPDATE widgets SET name = ?, feed = ?, backgroundColor = ?, borderColor = ?, borderWidth = ?, device = ?, label = ? WHERE id = ? ', [widget.name, widget.feed, widget.datasets[0].backgroundColor, widget.datasets[0].borderColor, widget.datasets[0].borderWidth, widget.config.device, widget.config.label, widget.id], (err, res) => {
             if(err) return reject(err);
             resolve(res)
         })

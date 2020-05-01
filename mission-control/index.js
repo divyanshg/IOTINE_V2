97b96003b94608c269dbef07b5c8849a5f1c310f
divyanshg <divyanshg809@gmail.com>
2020-05-02 02:36:05 +0530

    go

--- mission-control/index.js
+++ mission-control/index.js
@@ -64,6 +64,7 @@ io.on('connection', function (socket) {
                 //dataCamp.updateFeed(msg.user, msg.deviceId, msg.feed, msg.value)
             })
         }else if(msg.feed.split("/")[0] != "$__VERSION"){
+            console.log(msg)
             io.to(msg.user).emit("DEV_VERSION", msg.value, msg.deviceId)
             client.publish(msg.deviceId + "/" + msg.feed + "/NON", msg.value)
         }else{

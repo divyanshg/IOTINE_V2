20030c9cdb534a00fd984e40a02bc9a5811ad400
divyanshg <divyanshg809@gmail.com>
2020-05-01 14:05:05 +0530

    test

--- dev2.py
+++ dev2.py
@@ -16,7 +16,6 @@ def on_message(client, userdata, message):
     print(message.topic, "/",msg)
 
 while True:
-    time.sleep(4)
     IOTINE.publish("tempo", str(random.randint(0, 200)), '')
     IOTINE.publish("humido", str(random.randint(0, 100)), '')
 

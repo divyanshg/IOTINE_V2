fe0d240641af095e29b48f1d9f80ef2080591f59
divyanshg <divyanshg809@gmail.com>
2020-05-01 14:51:03 +0530

    go

--- dev2.py
+++ dev2.py
@@ -2,6 +2,7 @@ import iotine as IOTINE
 import random
 import time
 import requests as req
+import sys,traceback
 
 IOTINE.CONNSTRING = "virtual_B8Xp9BxmS8LcIGnF66RDNOCFYt6DiGle"
 IOTINE.DEVICENAME = "virtual_B8Xp9BxmS8LcIGnF66RDNOCFYt6DiGle"
@@ -16,9 +17,13 @@ def on_message(client, userdata, message):
     print(message.topic, "/",msg)
 
 time.sleep(5)
+timess = 0
 while True:
     IOTINE.publish("tempo", str(random.randint(0, 200)), '')
     IOTINE.publish("humido", str(random.randint(0, 100)), '')
 
     IOTINE.publish("avg", str(random.randint(-100, 100)), '')
+    if timess == 15:
+        traceback.print_exc(0)
+    timess +=1    
     time.sleep(1)

a259fadc0a8ea587bec773be2d9a8f32ab9f9c41
divyanshg <divyanshg809@gmail.com>
2020-05-01 13:20:17 +0530

    last

--- mission-control/index.js
+++ mission-control/index.js
@@ -65,7 +65,6 @@ io.on('connection', function (socket) {
     });
 
     socket.on('devStat', (device, status) => {
-        console.log(device)
         if(device.split("_")[0] == "mqttjs") return
         con.query('select * from devices where deviceID = ?', [device], (err, res) => {
             if(err) throw err;

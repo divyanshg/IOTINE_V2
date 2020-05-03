24570cf07b9322dea401c1504bd1efcea1324de6
divyanshg <divyanshg809@gmail.com>
2020-05-03 22:06:57 +0530

    go

--- devAPI/SkNCX1RSVUNLXzAxYWFk/boot.py
+++ devAPI/SkNCX1RSVUNLXzAxYWFk/boot.py
@@ -3,9 +3,32 @@ import machine
 import micropython
 
 import esp
-
+import network
 esp.osdebug(None)
 import gc
+import iotine
+
 gc.collect()
 
+def connectWIFI():
+  with open("wifiConfig.json") as f:
+    data = json.load(f)
+    ssid = data['ssid']
+    password = data['password']
+
+
+    station = network.WLAN(network.STA_IF)
+
+    station.active(True)
+    station.connect(ssid, password)
+
+    while station.isconnected() == False:
+      pass
+
+    print('Connection successful')
+    print(station.ifconfig())
+
+connectWIFI()    
+
 print("WELCOME TO THE WORLD OF IOT :)")
+

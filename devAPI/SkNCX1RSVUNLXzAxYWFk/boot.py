8003de33b6c0dd1ce8db3c7de82a1a53c60a4bff
divyanshg <divyanshg809@gmail.com>
2020-05-03 14:48:12 +0530

    go

--- devAPI/SkNCX1RSVUNLXzAxYWFk/boot.py
+++ devAPI/SkNCX1RSVUNLXzAxYWFk/boot.py
@@ -3,24 +3,29 @@ import machine
 import micropython
 import network
 import esp
+import json
+
 esp.osdebug(None)
 import gc
 gc.collect()
 
-print("WELCOME TO THE WORLD OF IOT :()")
-ssid = 'home_wifi'
-password = 'dig22569'
+print("WELCOME TO THE WORLD OF Internet Of Things :)")
 #EXAMPLE IP ADDRESS
 #mqtt_server = '192.168.1.144'
+def connectWifi():
+  with open("wifiConfig.json") as f:
+      data = json.load(f)
+      ssid, password = data['ssid'], data['pass']
+      station = network.WLAN(network.STA_IF)
 
+      station.active(True)
+      station.connect(ssid, password)
 
-station = network.WLAN(network.STA_IF)
+      while station.isconnected() == False:
+          pass
 
-station.active(True)
-station.connect(ssid, password)
+      print('Connection successful')
+      print(station.ifconfig())
 
-while station.isconnected() == False:
-  pass
+connectWifi()
 
-print('Connection successful')
-print(station.ifconfig())

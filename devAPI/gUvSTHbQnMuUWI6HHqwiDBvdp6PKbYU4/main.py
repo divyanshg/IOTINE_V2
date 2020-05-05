1d6fac972809c80f9f3a957327cdfcf7bc3a4d7b
divyanshg <divyanshg809@gmail.com>
2020-05-05 20:01:58 +0530

    go

--- devAPI/gUvSTHbQnMuUWI6HHqwiDBvdp6PKbYU4/main.py
+++ devAPI/gUvSTHbQnMuUWI6HHqwiDBvdp6PKbYU4/main.py
@@ -40,6 +40,7 @@ def joystick(adc):
 
 def on_sub(topic, msg):
   iotine.listenToSystemCommands(topic, msg)
+  print(topic.decode())
   if topic.decode() == iotine.device_id+"/ESP_X/NON":
       #moveServo(int(msg.decode()))  
       print(msg)

4948cdafb16c3eea78674050e1e6b76a0d648662
divyanshg <divyanshg809@gmail.com>
2020-05-06 23:33:39 +0530

    test

--- devAPI/gUvSTHbQnMuUWI6HHqwiDBvdp6PKbYU4/main.py
+++ devAPI/gUvSTHbQnMuUWI6HHqwiDBvdp6PKbYU4/main.py
@@ -59,20 +59,20 @@ iotine.subscribe("ESP_INT_LED", on_sub)
 def main_loop():
   if sw.value() == 0:
       button_pressed()
-  #iotine.publish(
-  #    [
-  #      {
-  #        "name": "ESP_X",
-  #        "value": joystick(adcx)
-  #      },
-  #      {
-  #        "name":"ESP_Y",
-  #        "value": joystick(adcy)
-  #      }
-  #    ]
-  #, on_pub)     
+  iotine.publish(
+      [
+        {
+          "name": "ESP_X",
+          "value": joystick(adcx)
+        },
+        {
+          "name":"ESP_Y",
+          "value": joystick(adcy)
+        }
+      ]
+  , on_pub)     
   iotine.checkMsg()   
-  if iotine.pubstop == False:
+  if iotine.pubstop == True:
     moveServo(joystick(adcx))
 
 iotine.loop(main_loop)

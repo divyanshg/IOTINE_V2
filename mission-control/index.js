2b639da9589e841511e1c281ab8d82182084af3f
divyanshg <divyanshg809@gmail.com>
2020-05-07 15:46:53 +0530

    test

--- mission-control/index.js
+++ mission-control/index.js
@@ -47,15 +47,12 @@ function encrypt(text) {
     let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(key), iv);
     let encrypted = cipher.update(text);
     encrypted = Buffer.concat([encrypted, cipher.final()]);
-    return {
-        iv: iv.toString('hex'),
-        encryptedData: encrypted.toString('hex')
-    };
+    return encrypted.toString('hex')
 }
 
 function decrypt(text) {
-    let iv = Buffer.from(text.iv, 'hex');
-    let encryptedText = Buffer.from(text.encryptedData, 'hex');
+    let iv = Buffer.from(iv, 'hex');
+    let encryptedText = Buffer.from(text, 'hex');
     let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(key), iv);
     let decrypted = decipher.update(encryptedText);
     decrypted = Buffer.concat([decrypted, decipher.final()]);

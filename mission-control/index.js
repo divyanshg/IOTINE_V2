63f5040ab7fb239619210ac4dd6089581df62165
divyanshg <divyanshg809@gmail.com>
2020-05-07 16:02:52 +0530

    test

--- mission-control/index.js
+++ mission-control/index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 const crypto = require('crypto');
 const algorithm = 'aes-256-cbc';
-const key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8";
+const key = crypto.randomBytes(32);
 const iv = "2hg34o09j09d23JJ";
 
 const options = {

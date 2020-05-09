c89ac757ddf3bb3a0015efa46e928eed488c0d44
divyanshg <divyanshg809@gmail.com>
2020-05-09 20:48:49 +0530

    test

--- dev1/dev1.py
+++ dev1/dev1.py
@@ -14,11 +14,14 @@ def on_message(client, userdata, message):
 
 time.sleep(5)
 timess = 0
+
+IOTINE.subscribe("CORE_TEMP", on_message)
+
 while True:
     IOTINE.publish("ENGINE_OIL", str(random.randint(0, 200)), '')
     IOTINE.publish("ENGINE_TEMPERATURE", str(random.randint(0, 100)), '') 
     IOTINE.doDefaults()
     IOTINE.publish("CONT_TEMP", str(random.randint(1, 100)), '')
-    #IOTINE.publish("CORE_TEMP", str(random.randint(1, 100)), '')
+    IOTINE.publish("CORE_TEMP", str(random.randint(1, 100)), '')
     IOTINE.publish("TYRE_PRESSURE_AVG", str(random.randint(1, 100)), '')
     time.sleep(1)

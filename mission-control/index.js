b5966f580853b414f780a5bcc6a2bdea267c7648
divyanshg <divyanshg809@gmail.com>
2020-05-11 14:41:50 +0530

    test

--- mission-control/index.js
+++ mission-control/index.js
@@ -114,7 +114,7 @@ io.on('connection', function (socket) {
                                 if (err) return err
 
                                 //Checking and running the events processing
-                                if (feedInfo[0].events != []) {
+                                if (feedInfo[0].events != "[]") {
                                     var events = JSON.parse(feedInfo[0].events)
 
                                     events.forEach(event => {

cf3953333ab8bb3731ac4f8b4752d4fb6667a89d
divyanshg <divyanshg809@gmail.com>
2020-05-11 18:17:32 +0530

    test

--- events/functions/iub54i6bibu64/printRandomValue/index.js
+++ events/functions/iub54i6bibu64/printRandomValue/index.js
@@ -1,5 +1,5 @@
 'use strict'
 
 exports.handler = async(event) => {
-    return Math.floor(Math.random() * parseInt(event.msg))
+    return console.log(Math.floor(Math.random() * parseInt(event.msg)))
 }

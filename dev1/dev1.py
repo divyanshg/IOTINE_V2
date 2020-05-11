fe7d5c07e6a13fc5756864ffc9bfa6d61df68acf
divyanshg <divyanshg809@gmail.com>
2020-05-11 14:37:10 +0530

    test

--- dev1/dev1.py
+++ dev1/dev1.py
@@ -15,7 +15,27 @@ while True:
 
     IOTINE.publish([
         {
+            "name":"ENGINE_OIL",
+            "value": str(random.randint(0, 200)),
+            "callback": ""
+        },
+        {
+            "name": "ENGINE_TEMPERATURE",
+            "value": str(random.randint(0, 100)),
+            "callback": ""
+        },
+        {
+            "name":"CONT_TEMP",
+            "value": str(random.randint(1, 100)),
+            "callback": ""
+        },
+        {
             "name":"CORE_TEMP",
+            "value": str(random.randint(1, 100)),
+            "callback": ""
+        },
+        {
+            "name":"TYRE_PRESSURE_AVG",
             "value": str(random.randint(1, 100)),
             "callback": ""
         }

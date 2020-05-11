9bfe9939543e559685ad689cf1cc1c06741dae3b
divyanshg <divyanshg809@gmail.com>
2020-05-11 14:00:29 +0530

    ttest

--- events/functions/iub54i6bibu64/sendamail/index.js
+++ events/functions/iub54i6bibu64/sendamail/index.js
@@ -2,32 +2,35 @@
 
 exports.handler = function (event, callback) {
     var nodemailer = require('nodemailer');
-    
+
     var payload = parseInt(event.msg);
     var timestamp = parseInt(event.timestamp);
 
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'divg809@gmail.com',
-            pass: 'div21902'
-        }
-    });
+    if (payload >= 50) {
 
-    var mailOptions = {
-        from: 'divg809@gmail.com',
-        to: 'divyanshg809@gmail.com',
-        subject: 'Core temperature was high',
-        text: `Core temperature reached ${payload} at ${timestamp}`
-    };
-
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            return
-        } else {
-            return
-        }
-    });
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: 'divg809@gmail.com',
+                pass: 'div21902'
+            }
+        });
+    
+        var mailOptions = {
+            from: 'divg809@gmail.com',
+            to: 'divyanshg809@gmail.com',
+            subject: 'Core temperature was high',
+            text: `Core temperature reached ${payload} at ${timestamp}`
+        };
+        
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                return
+            } else {
+                return
+            }
+        });
+    }
 
     if (callback == null || callback == '') return data
     callback(null, data);

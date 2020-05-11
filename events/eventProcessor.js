b8c569d2826534301d5ff809bf0155ef7bfc96d7
divyanshg <divyanshg809@gmail.com>
2020-05-11 17:22:07 +0530

    test

--- events/eventProcessor.js
+++ events/eventProcessor.js
@@ -7,9 +7,7 @@ exports.processEvent = (uModule, inputs) => {
             Object.keys(require.cache).forEach(function(key) { delete require.cache[key] })
             var mod = require(`./functions/${uModule}/index`)
 
-            var response = mod.handler(inputs)
-
-            resolve(String(response))
+            resolve(String(mod.handler(inputs)))
 
         } catch (e) {
             reject("Unable to process event <br> " + e)

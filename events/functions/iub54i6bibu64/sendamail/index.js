cd86fb303bd566e9da520c4c1a482b766db03472
divyanshg <divyanshg809@gmail.com>
2020-05-11 17:27:09 +0530

    oops

--- events/functions/iub54i6bibu64/sendamail/index.js
+++ events/functions/iub54i6bibu64/sendamail/index.js
@@ -1,16 +1,8 @@
 'use strict'
 
-
-
-var iotine = require('./iotine')
-iotine = iotine.iotine;
-
 var stateCheck = 0;
 
 exports.handler = async (event) => {
-
-
-    iotine.configMail({emailAddress: 'iotine.alert@gmail.com', password: 'div21902', service: 'gmail'})
 
     var payload = parseInt(event.msg);
     var mtime = event.timestamp;

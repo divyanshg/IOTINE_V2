20e3068e0a0db09e573f6554fa200dc3a9e1ca8d
divyanshg <divyanshg809@gmail.com>
2020-05-13 18:32:47 +0530

    test

--- mission-control/mission.js
+++ mission-control/mission.js
@@ -128,7 +128,7 @@ async function loadPROPS(dev) {
         if (this.readyState == 4 && this.status == 200) {
             // Typical action to be performed when the document is ready:
             var props = JSON.parse(xhttp.responseText)
-            loadFeeds(devID, ".tFeeds")
+            loadFeeds(devID)
 
             mnam.innerHTML = props[0].dName;
             dn.innerHTML = props[0].dName;
@@ -144,12 +144,9 @@ async function loadPROPS(dev) {
     $(".devPROPERTIES").toggle()
 
 }
-async function loadFeeds(dev, elm) {
-    var tfds = document.querySelector(elm)
-    if (elm = ".tFeeds") {
-        $(elm).html('<tr><th>FEEDS</th></tr><tr><th>NAME</th><th>UNIT</th></tr>')
-    }
-
+async function loadFeeds(dev) {
+    var tfds = document.querySelector(".tFeeds")
+    $(".tFeeds").html('<tr><th>FEEDS</th></tr><tr><th>NAME</th><th>UNIT</th></tr>')
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
@@ -181,8 +178,7 @@ async function loadFeeds(dev, elm) {
 }
 
 async function histloadFeeds(elm) {
-    var tfds = document.querySelector(elm)
-    var dev = tfds.value.split("@")[1]
+    var dev = elm.value.split("@")[1]
 
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {

56c511840804afff09bd8eb6b4326a81bdc59d51
divyanshg <divyanshg809@gmail.com>
2020-05-13 23:34:36 +0530

    test

--- mission-control/mission.js
+++ mission-control/mission.js
@@ -56,6 +56,7 @@ var dashSettings = new River({
                 root.style.setProperty("--card-btn", "rgb(49, 49, 49)")
                 root.style.setProperty("--backDrop", "#0b0b0bcc")
                 root.style.setProperty("--shadow", "0 12px 28px 0 #00000033, 0 2px 4px 0 #0000001a, inset 0 0 0 1px #ffffff0d")
+                root.style.setProperty("--active", "#4e4e4e")
 
                 document.querySelector(".darkCheck").className += " checked"
 
@@ -75,6 +76,7 @@ var dashSettings = new River({
                 root.style.setProperty("--card-btn", "#f3f3f3")
                 root.style.setProperty("--backDrop", "#f4f4f4cc")
                 root.style.setProperty("--shadow", "0 12px 28px 0 #00000033, 0 2px 4px 0 #0000001a, inset 0 0 0 1px #ffffff80")
+                root.style.setProperty("--active", "#d2d2d2")
 
                 document.querySelector(".darkCheck").classList.remove("checked")
             }

655ae4026464618febedf835987d5202584cf8e0
divyanshg <divyanshg809@gmail.com>
2020-05-17 22:22:27 +0530

    test

--- deviceRegistrar/index.js
+++ deviceRegistrar/index.js
@@ -21,6 +21,11 @@ app.use(cors())
 app.post('/certificateUpload/:device', (req, res) => {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
+
+        if (!fs.existsSync(`/var/www/html/IOTINE_V2/Collector/certificates/${req.params.device}/`)) {
+            fs.mkdirSync(`/var/www/html/IOTINE_V2/Collector/certificates/${req.params.device}/`);
+        }
+        
         var oldpath = files.filetoupload.path;
         var newpath = `/var/www/html/IOTINE_V2/Collector/certificates/${req.params.device}/${files.filetoupload.name}`;
         fs.rename(oldpath, newpath, function (err) {

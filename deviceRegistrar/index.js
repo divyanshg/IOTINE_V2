2aac784f6b03ce7e8c0a550caa9149c3202a32d9
divyanshg <divyanshg809@gmail.com>
2020-05-17 21:56:22 +0530

    pathChange

--- deviceRegistrar/index.js
+++ deviceRegistrar/index.js
@@ -22,7 +22,7 @@ app.post('/certificateUpload/:device', (req, res) => {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
         var oldpath = files.filetoupload.path;
-        var newpath = `/var/www/html/IOTINE_V2/devAPI/${req.params.device}/${files.filetoupload.name}`;
+        var newpath = `/var/www/html/IOTINE_V2/Collector/certificates/${req.params.device}/${files.filetoupload.name}`;
         fs.rename(oldpath, newpath, function (err) {
             if (err) throw err;
             res.write('File uploaded and moved!');

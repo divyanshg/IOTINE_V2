7e90aa60f3c3552c0ba81cfa6683550a5134f510
divyanshg <divyanshg809@gmail.com>
2020-05-17 21:28:07 +0530

    test

--- deviceRegistrar/index.js
+++ deviceRegistrar/index.js
@@ -7,7 +7,7 @@ http.createServer(function (req, res) {
         var form = new formidable.IncomingForm();
         form.parse(req, function (err, fields, files) {
             var oldpath = files.filetoupload.path;
-            var newpath = files.filetoupload.name;
+            var newpath = `/var/www/html/IOTINE_V2/devAPI/sampleDevice/${files.filetoupload.name}`;
             fs.rename(oldpath, newpath, function (err) {
                 if (err) throw err;
                 res.write('File uploaded and moved!');

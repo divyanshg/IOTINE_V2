fdd41107070e189bd8412759821e8ce9442974c8
divyanshg <divyanshg809@gmail.com>
2020-05-17 23:50:49 +0530

    test

--- mission-control/index.js
+++ mission-control/index.js
@@ -131,6 +131,7 @@ io.on('connection', function (socket) {
                             if (err) return err;
                         });
                     } else {
+                        console.log(msg.unit)
                         con.query('select unit, events, time from feed_vals where user_id = ? and deviceID = ? and name =? limit 1', [msg.user, msg.deviceId, msg.feed], async (err, feedInfo) => {
                             if (err) return err;
                             con.query('UPDATE feed_vals SET value =? WHERE user_id=? AND deviceID=? AND name=?', [msg.value, msg.user, msg.deviceId, msg.feed], async (err, res) => {

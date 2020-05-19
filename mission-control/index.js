baba6c3a10c06ebb66db837ff946ba89c63a3fe1
divyanshg <divyanshg809@gmail.com>
2020-05-19 20:43:17 +0530

    tesst

--- mission-control/index.js
+++ mission-control/index.js
@@ -11,7 +11,8 @@ const iv = "2hg34o09j09d23JJ";
 
 const options = {
     key: fs.readFileSync('./localhost.key'),
-    cert: fs.readFileSync('./localhost.crt')
+    cert: fs.readFileSync('./localhost.crt'),
+    ca:fs.readFileSync('./ca_bundle.crt')
 };
 
 var http = require('https')

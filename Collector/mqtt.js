a197a89f84b6cb7fa87cbccab526cdd7deb6ab4f
divyanshg <divyanshg809@gmail.com>
2020-05-19 22:00:34 +0530

    test

--- Collector/mqtt.js
+++ Collector/mqtt.js
@@ -8,7 +8,7 @@ var jwtDecode = require('jwt-decode');
 const axios = require('axios')
 
 var con = mysql.createConnection({
-    host: "https://iotine.ddns.net:3306",
+    host: "iotine.ddns.net",
     user: "divyanshg21",
     password: "potty_khale",
     database: "fila_iot"
